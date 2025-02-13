4f16cfe14f626f767615caf706b7585a34828df0
muhsin <muhsintp.develop@gmail.com>
2025-02-13 19:14:17 +0530

    add contact ,Testimonials,

---
+++ assets/javascript/script.js
@@ -0,0 +1,50 @@
+$(document).ready(function () {
+    $(".testimonials-carousel").owlCarousel({
+        loop: true,
+        margin: 20,
+        nav: true,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        autoplayHoverPause: true,
+        navText: [
+            "<i class='fas fa-chevron-left'></i>",
+            "<i class='fas fa-chevron-right'></i>"
+        ],
+        responsive: {
+            0: {
+                items: 1,
+                nav: false
+            },
+            768: {
+                items: 2,
+                nav: true
+            },
+            1024: {
+                items: 3,
+                nav: true
+            }
+        }
+    });
+});
+
+
+let scrollTop = document.querySelector('.scroll-top');
+
+function toggleScrollTop() {
+  if (scrollTop) {
+    window.scrollY > 100 ? scrollTop.classList.add('active') : scrollTop.classList.remove('active');
+  }
+}
+scrollTop.addEventListener('click', (e) => {
+  e.preventDefault();
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
+
+window.addEventListener('load', toggleScrollTop);
+document.addEventListener('scroll', toggleScrollTop);
+
+
+
